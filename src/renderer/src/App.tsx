93797613311b9467a61076df488fe2ee4ee6df40
Aczell Bien Florencio <32163395+keenplify@users.noreply.github.com>
2026-09-09 01:29:30 +0800

    Mount server reconnect banner

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,12 @@
 import { AuthPage } from './features/auth/AuthPage'
+import { ConnectionBanner } from './features/matchmaking/ConnectionBanner'
 import { UpdateBanner } from './features/updates/UpdateBanner'
 
 function App(): React.JSX.Element {
   return (
     <>
       <UpdateBanner />
+      <ConnectionBanner />
       <AuthPage />
     </>
   )
