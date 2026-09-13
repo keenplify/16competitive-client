d7d5aa96880153ac2d7e49c80acf9d7a9b858dc6
keenplify <keenplify@gmail.com>
2026-09-14 06:20:06 +0800

    Fix glow

--- src/renderer/src/features/matchmaking/LobbyPage.tsx
+++ src/renderer/src/features/matchmaking/LobbyPage.tsx
@@ -4,7 +4,7 @@ import { LobbyNavigation } from '../../components/ui/lobby/Navigation'
 import { useAuthStore } from '../auth/auth.store'
 import { PartyInvitationModal } from '../party/PartyInvitationModal'
 import { PartyChat } from '../party/PartyChat'
-import { modelForSlot } from '../party/party-models'
+import { modelForSlot, presentationModelPath } from '../party/party-models'
 import { PartyModelScene } from '../party/PartyModelScene'
 import { LobbySocialSidebar } from '../party/LobbySocialSidebar'
 import { usePartyStore } from '../party/party.store'
@@ -64,16 +64,18 @@ const LobbyScene = memo(function LobbyScene({ player, party }: LobbySceneProps):
         <PartyModelScene
           actors={members.map((member, index) => {
             const isCurrentPlayer = member.id === player.id
+            const weaponKey = isCurrentPlayer ? lobbyWeaponKey : member.lobbyWeaponKey
+            const selectedModelPath = isCurrentPlayer
+              ? lobbyPlayerModel
+              : (member.lobbyPlayerModel ?? modelForSlot(index, member))
             return {
               member,
-              modelPath: isCurrentPlayer
-                ? lobbyPlayerModel
-                : (member.lobbyPlayerModel ?? modelForSlot(index, member)),
+              modelPath: presentationModelPath(selectedModelPath, weaponKey),
               fallbackModelPath: modelForSlot(index, member),
               weaponPath: isCurrentPlayer
                 ? (lobbyWeaponModelPath ?? defaultWeaponModelPath(lobbyWeaponKey))
                 : (member.lobbyWeaponModelPath ?? defaultWeaponModelPath(member.lobbyWeaponKey)),
-              weaponKey: isCurrentPlayer ? lobbyWeaponKey : member.lobbyWeaponKey,
+              weaponKey,
               isLeader: party?.leaderId === member.id,
               isCurrentPlayer
             }
