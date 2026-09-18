0d04b9b0e1be868ecd936ceeaef37098781985d7
Aczell Bien Florencio <32163395+keenplify@users.noreply.github.com>
2026-09-19 00:30:26 +0800

    Fix CountryFlag lint directive formatting

--- src/renderer/src/components/CountryFlag.tsx
+++ src/renderer/src/components/CountryFlag.tsx
@@ -6,7 +6,9 @@ export interface CountryOption {
   name: string
 }
 
-// Country options are static data shared by profile controls.\n// eslint-disable-next-line react-refresh/only-export-components\nexport const COUNTRY_OPTIONS: CountryOption[] = [
+// Country options are static data shared by profile controls.
+// eslint-disable-next-line react-refresh/only-export-components
+export const COUNTRY_OPTIONS: CountryOption[] = [
   { code: 'AF', name: "Afghanistan" },
   { code: 'AX', name: "Åland Islands" },
   { code: 'AL', name: "Albania" },
